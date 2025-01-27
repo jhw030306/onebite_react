8966c7dfeb74b02ff237bfd2dc3935671e02e895
jhw030306 <154288326+jhw030306@users.noreply.github.com>
2025-01-27 10:32:25 +0900

    Add files via upload

---
+++ section-2/chapter13.js
@@ -0,0 +1,33 @@
+function add10(num) {
+  const promise = new Promise((resolve, reject) => {
+    // 비동기 작업
+    // executor
+
+    setTimeout(() => {
+      if (typeof num === "number") {
+        resolve(num + 10);
+      } else {
+        reject("num이 숫자가 아닙니다");
+      }
+    }, 2000);
+  });
+
+  return promise;
+}
+
+add10(0)
+  .then((result) => {
+    console.log(result);
+
+    return add10(result);
+  })
+  .then((result) => {
+    console.log(result);
+    return add10(undefined);
+  })
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
